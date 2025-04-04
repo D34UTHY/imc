e0bbe83646bbd0fcd284a860a8a6f3c6e8dae48f
Matheus Sant'Ana <matheuzinhu.santana@gmail.com>
2025-04-03 21:22:09 -0300

    Coloquei o emoji da calculadora antes do titulo da página e do titulo e modifiquei o Javascript pois estava dando as classificações erradas

--- src/script.js
+++ src/script.js
@@ -11,13 +11,16 @@ function calcularIMC() {
             indice = "Abaixo do peso"
         } else if (imc > 18.5 && imc < 24.99) {
             indice = "Peso normal"
-        } else if (imc > 24.99 && imc < 34.99) {
+        } else if (imc > 25.00 && imc < 29.99) {
+            indice = "Sobrepeso"
+        } else if (imc > 30 && imc < 34.99) {
             indice = "Obesidade grau I"
-        } else if (imc > 34.99 && imc < 39.99) {
+        } else if (imc > 35.00 && imc < 39.99) {
             indice = "Obesidade grau II"
         } else {
             indice = "Obesidade grau III"
         }
+        
 
         resultado.innerHTML = `<p>Seu IMC é: ${imc.toFixed(1)}</p>
                                <p>Classificação: ${indice}</p>`
